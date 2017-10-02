66fadeade5c85de955db35a7058e41eedd39ac68
DESKTOP-6O5F3M6\Gabriel PC <g_e_ramirez@outlook.com>
2017-10-02 01:08:45 -0500

    Graphic Interface basic model

--- parcial.py
+++ parcial.py
@@ -2,69 +2,153 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import numpy as np
 import scipy
-f_x = []
-f_y = []
-f_m = []
-f_c = []
-def m(p1, p2):
-    return (p2[1] - p1[1])/(p2[0] - p1[0])
-    pass
-
-def linear_f(p1, x2, pendiente):
-    return pendiente*(x2 - p1[0]) + p1[1]
-    pass
-
-def get_cubic_correction(p, fm, code):
-    z2 = (6*(fm[2]*p[1][0] + fm[1]*p[2][0] - fm[2]*p[2][0] + 2*fm[1]*p[3][0] + 2*fm[0]*p[1][0] - 2*fm[0]*p[3][0] - 3*fm[1]*p[1][0]) + 0.0)/(4*(p[0][0]*p[1][0] + p[2][0]*p[3][0] - p[0][0]*p[3][0]) - pow(p[1][0] + p[2][0], 2))  
-    z3 = (6*(fm[1]*p[1][0] + fm[0]*p[2][0] - fm[0]*p[1][0] + 2*fm[1]*p[0][0] + 2*fm[2]*p[2][0] - 2*fm[2]*p[0][0] - 3*fm[1]*p[2][0]) + 0.0)/(4*(p[0][0]*p[1][0] + p[2][0]*p[3][0] - p[0][0]*p[3][0]) - pow(p[1][0] + p[2][0], 2))
-    a = -1
-    b = -1
-    if code == 0:
-        a = z2/(6*(p[0][0] - p[1][0]))
-        b = 2*z2/(6*(p[1][0] - p[0][0]))
-    if code == 1:
-        a = (2*z2 + z3)/(6*(p[1][0] - p[2][0]))
-        b = (2*z3 + z2)/(6*(p[2][0] - p[1][0]))
-    if code == 2:
-        a = z3/(3*(p[2][0] - p[3][0]))
-        b = z3/(6*(p[3][0] - p[2][0]))
-    return [a, b]
-    pass
-
-def cubic_corrected(a, b, p, x, code):
-    return a*pow(x - p[code + 1][0], 2)*(x - p[code][0]) + b*(x - p[code + 1][0])*pow(x - p[code][0], 2)
-    pass
-
-def interpolate(p):
-    for i in range(0, p.__len__() - 1):
-        p1 = p[i]
-        p2 = p[i+1]
-        pendiente = m(p1, p2)
-        f_m.append(pendiente)
-        count = p1[0]
-        while count < p2[0]:
-            f_x.append(count)
-            f_y.append(linear_f(p1, count, pendiente))
-            count += 0.01
-    for i in range(0, p.__len__() - 1):
-        p1 = p[i]
-        p2 = p[i+1]
-        correct = get_cubic_correction(p, f_m, i)
-        count = p1[0]
-        while count < p2[0]:
-            f_c.append((cubic_corrected(correct[0], correct[1], p, count, i)))
-            count += 0.01
-    for i in range(0, f_y.__len__()):
-        f_y[i]  += f_c[i]
-    pass
-
-p = [(0, 0), (2, 4), (3, 3), (4, 2)]
-interpolate(p)
-plt.axis([0, 5, 0, 5])
-plt.plot(f_x, f_y)
-plt.savefig('spline.png')
 
 
+from qtpy.QtGui import *
+import qtpy.QtWidgets as wid
+import sys
+
+
+def window():
+   #Variables
+   data = []
+   txt = ""
+   app = wid.QApplication(sys.argv)
+   w = wid.QWidget()
+   w.setGeometry(200,200,600,600)
+   w.setWindowTitle("Trabajo de Algebra lineal: Splines Cubicos")
+   #Labels and text   
+   
+   
+   
+   #Title
+   l = wid.QLabel(w)
+   l.setText("Trabajo de Algebra Lineal!")
+   l.move(170,50)
+   l.setFont(QFont('SansSerif', 20))
+   
+   #information
+   l1 = wid.QLabel(w)
+   l1.setText("Agrega la cantidad de puntos hasta que desee generar el spline")
+   l1.move(30,100)
+
+
+   
+   #Textbox
+   lx = wid.QLabel(w)
+   lx.setText("Inserte valor en X: ")
+   lx.move(30,120)
+   tx = wid.QLineEdit(w)
+   tx.setGeometry(130,115,50,20)
+   
+   ly = wid.QLabel(w)
+   ly.setText("Inserte valor en Y: ")
+   ly.move(30,150)
+   ty = wid.QLineEdit(w)
+   ty.setGeometry(130,145,50,20)
+   
+   i1 = wid.QLabel(w)
+   i1.setPixmap(QPixmap("data.png"))
+   i1.move(200,200)
+      #Functions to add
+   def fill():
+       temp = [int(tx.text()),int(ty.text())]
+       data.append(temp)
+       pass
+   
+   def get_data():
+    
+        
+       pass
+        
+   
+    #Boton 1
+   btn = wid.QPushButton(w)
+   btn.setText("Click Me!")
+   btn.move(200,140)
+   btn.clicked.connect(fill)
+   #Positions on the map
+   
+   
+
+    
+   f_x = []
+   f_y = []
+   f_m = []
+   f_c = []
+    
+    
+    
+    
+    #CODE
+    
+   def m(p1, p2):
+       return (p2[1] - p1[1])/(p2[0] - p1[0])
+       pass
+    
+   def linear_f(p1, x2, pendiente):
+       return pendiente*(x2 - p1[0]) + p1[1]
+       pass
+    
+   def get_cubic_correction(p, fm, code):
+       z2 = (6*(fm[2]*p[1][0] + fm[1]*p[2][0] - fm[2]*p[2][0] + 2*fm[1]*p[3][0] + 2*fm[0]*p[1][0] - 2*fm[0]*p[3][0] - 3*fm[1]*p[1][0]) + 0.0)/(4*(p[0][0]*p[1][0] + p[2][0]*p[3][0] - p[0][0]*p[3][0]) - pow(p[1][0] + p[2][0], 2))  
+       z3 = (6*(fm[1]*p[1][0] + fm[0]*p[2][0] - fm[0]*p[1][0] + 2*fm[1]*p[0][0] + 2*fm[2]*p[2][0] - 2*fm[2]*p[0][0] - 3*fm[1]*p[2][0]) + 0.0)/(4*(p[0][0]*p[1][0] + p[2][0]*p[3][0] - p[0][0]*p[3][0]) - pow(p[1][0] + p[2][0], 2))
+       a = -1
+       b = -1
+       if code == 0:
+           a = z2/(6*(p[0][0] - p[1][0]))
+           b = 2*z2/(6*(p[1][0] - p[0][0]))
+       if code == 1:
+           a = (2*z2 + z3)/(6*(p[1][0] - p[2][0]))
+           b = (2*z3 + z2)/(6*(p[2][0] - p[1][0]))
+       if code == 2:
+           a = z3/(3*(p[2][0] - p[3][0]))
+           b = z3/(6*(p[3][0] - p[2][0]))
+       return [a, b]
+       pass
+    
+   def cubic_corrected(a, b, p, x, code):
+       return a*pow(x - p[code + 1][0], 2)*(x - p[code][0]) + b*(x - p[code + 1][0])*pow(x - p[code][0], 2)
+       pass
+    
+   def interpolate(p):
+       for i in range(0, p.__len__() - 1):
+           p1 = p[i]
+           p2 = p[i+1]
+           pendiente = m(p1, p2)
+           f_m.append(pendiente)
+           count = p1[0]
+           while count < p2[0]:
+               f_x.append(count)
+               f_y.append(linear_f(p1, count, pendiente))
+               count += 0.01
+       for i in range(0, p.__len__() - 1):
+           p1 = p[i]
+           p2 = p[i+1]
+           correct = get_cubic_correction(p, f_m, i)
+           count = p1[0]
+           while count < p2[0]:
+               f_c.append((cubic_corrected(correct[0], correct[1], p, count, i)))
+               count += 0.01
+       for i in range(0, f_y.__len__()):
+           f_y[i]  += f_c[i]
+       pass
+
+   def draw(p):
+       interpolate(p)
+       plt.axis([0, 5, 0, 5])
+       plt.plot(f_x, f_y)
+       plt.savefig('spline.png')   
+       return
+
+   p = [(0, 0), (2, 4), (3, 3), (4, 2)]
+    
+   w.show()
+  
+   sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    window()
 
 
 
